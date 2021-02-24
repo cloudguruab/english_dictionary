bcc081c1673ec1744ccd2358c8eb449d5b9d5e8b
cloudguruab <ap.brown011@gmail.com>
2021-02-23 23:36:12 -0500

    init 2

--- eng_dictionary/settings.py
+++ eng_dictionary/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'kd-j4(rk46-bw0mve7g@ldu3bc=n+y&sit@frr#no6u23icyia'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['herokueng_dict_app.herokuapp.com']
+ALLOWED_HOSTS = ['herokueng-dict-app.herokuapp.com']
 
 
 # Application definition
